9592d06189e6d4360396b45ca1284c92772fef8e
Jake Siddall <jake.siddall@gmail.com>
2021-04-29 10:15:10 -0700

    Update to Gatsby blog

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":630,"linkImagesToOriginal":true,"showCaptions":false,"markdownCaptions":false,"sizeByPixelDensity":false,"backgroundColor":"white","quality":50,"withWebp":false,"tracedSVG":false,"loading":"lazy","disableBgImageOnAlpha":false,"disableBgImage":false},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Gatsby Starter Blog","short_name":"GatsbyJS","start_url":"/","background_color":"#ffffff","theme_color":"#663399","display":"minimal-ui","icon":"src/images/hand-wave.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"d433c84c68c7861f0a500bb1f528ce16"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
